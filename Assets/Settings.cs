1de82b8dbe606403315c9429c918510fb3e62d57
jordiprats <jordi.prats@gmail.com>
2018-12-09 18:35:51 +0100

    varis load

---
+++ Assets/Settings.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class Settings
+{
+    private static int cards = 5;
+
+    public static int Cards
+    {
+        get 
+        {
+            return cards;
+        }
+        set 
+        {
+            cards = value;
+        }
+    }
+
+	private static int level = 0;
+
+    public static int Level
+    {
+        get 
+        {
+            return level;
+        }
+        set 
+        {
+            level = value;
+        }
+    }
+
+}
