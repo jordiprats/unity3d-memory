267fd92e2a7bc97ae9433a5fc4a02015230f14ad
jordiprats <jordi.prats@gmail.com>
2018-12-27 18:55:34 +0100

    funcionalitat de flip

--- Assets/Scripts/MemoryController.cs
+++ Assets/Scripts/MemoryController.cs
@@ -9,8 +9,11 @@ public class MemoryController : MonoBehaviour {
     //recursos
     public Object[] fotos;
     public Object[] noms;
+	public Object[] backs;
 
 	public Object[] llista_sprites;
+
+	public GameObject[] llista_gameobjs;
 
     //ordre
 	public int[] llista_alumnes;
@@ -55,6 +58,7 @@ public class MemoryController : MonoBehaviour {
 
 		fotos = Resources.LoadAll(path: "Fotos", systemTypeInstance: typeof(Sprite));
         noms = Resources.LoadAll(path: "Noms", systemTypeInstance: typeof(Sprite));
+		backs = Resources.LoadAll(path: "Backs", systemTypeInstance: typeof(Sprite));
 
 		Debug.Log("level#: " + Settings.Level);
 		Debug.Log("card#: " + Settings.Cards);
@@ -76,6 +80,7 @@ public class MemoryController : MonoBehaviour {
 
 		this.llista_cartes = new int[Settings.Cards*2];
 		this.llista_sprites = new Object[Settings.Cards*2];
+		this.llista_gameobjs = new GameObject[Settings.Cards*2];
 
 		Debug.Log("Settings cartes: "+Settings.Cards);
 
@@ -158,30 +163,66 @@ public class MemoryController : MonoBehaviour {
 
 		for (int i = 0; i < this.llista_sprites.Length; i++)
 		{
-			GameObject newGO=new GameObject(this.llista_cartes[i]+" "+this.llista_sprites[i].name);
-			newGO.AddComponent(typeof(SpriteRenderer));
-			newGO.AddComponent(typeof(RectTransform));
-			newGO.AddComponent(typeof(BoxCollider2D));
+			GameObject item = new GameObject(this.llista_cartes[i]+" "+this.llista_sprites[i].name);
+			item.AddComponent(typeof(RectTransform));
+			item.AddComponent(typeof(BoxCollider2D));
+			item.AddComponent(typeof(FlipController));
+			item.transform.SetParent(cardholder_glg.transform);
+
+
+			GameObject card_front=new GameObject(this.llista_cartes[i]+" "+this.llista_sprites[i].name+" front");
+			card_front.AddComponent(typeof(SpriteRenderer));
+			card_front.AddComponent(typeof(RectTransform));
 
 			//newGO.transform.parent = cardholder_glg.transform;
-			newGO.transform.SetParent(cardholder_glg.transform);
+			card_front.transform.SetParent(item.transform);
          	//newGO.transform.localScale = new Vector3( 1, 1, 1 );
          	//newGO.transform.localPosition = Vector3.zero;
 
-			newGO.GetComponent<SpriteRenderer>().sprite = this.llista_sprites[i] as Sprite;
-			newGO.GetComponent<SpriteRenderer>().drawMode = SpriteDrawMode.Sliced;
+			card_front.GetComponent<SpriteRenderer>().sprite = this.llista_sprites[i] as Sprite;
+			card_front.GetComponent<SpriteRenderer>().drawMode = SpriteDrawMode.Sliced;
             //newGO.GetComponent<SpriteRenderer>().size += new Vector2(0.05f, 0.01f);
 			//newGO.GetComponent<SpriteRenderer>().size = new Vector2(5,6);
 
-			newGO.GetComponent<BoxCollider2D>().size = newGO.GetComponent<SpriteRenderer>().sprite.bounds.size;
-			
+			item.GetComponent<BoxCollider2D>().size = card_front.GetComponent<SpriteRenderer>().sprite.bounds.size;
+
+			GameObject card_back=new GameObject(this.llista_cartes[i]+" "+this.llista_sprites[i].name+" back");
+			card_back.AddComponent(typeof(SpriteRenderer));
+			card_back.AddComponent(typeof(RectTransform));
+
+			//newGO.transform.parent = cardholder_glg.transform;
+			card_back.transform.SetParent(item.transform);
+         	//newGO.transform.localScale = new Vector3( 1, 1, 1 );
+         	//newGO.transform.localPosition = Vector3.zero;
+
+			card_back.GetComponent<SpriteRenderer>().sprite = this.backs[0] as Sprite;
+			card_back.GetComponent<SpriteRenderer>().drawMode = SpriteDrawMode.Sliced;
+            //newGO.GetComponent<SpriteRenderer>().size += new Vector2(0.05f, 0.01f);
+			//newGO.GetComponent<SpriteRenderer>().size = new Vector2(5,6);
+			card_back.transform.position = new Vector3(card_back.transform.position.x, card_back.transform.position.y, (float)-0.0001);
+
+			this.llista_gameobjs[i]=item;
 		}
 
 	}
 
 	void Update () 
 	{
-		if(Input.GetMouseButton(0))
+		FlipController hit_flip_controller;
+		FlipController selected_flip_controller;
+
+		Text hit_txt = (Text)GameObject.Find("hit").GetComponent(typeof(Text));
+		Text selected_txt = (Text)GameObject.Find("selected").GetComponent(typeof(Text));
+
+		
+		int flipping_counter=0;
+		for (int i = 0; i < this.llista_gameobjs.Length; i++)
+		{
+			if(((FlipController)(llista_gameobjs[i].GetComponent(typeof(FlipController)))).flipping)
+				flipping_counter++;
+		}
+
+		if(Input.GetMouseButton(0) && flipping_counter==0)
 		{
 			var inputPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
 			RaycastHit2D[] touches = Physics2D.RaycastAll(inputPosition, inputPosition, 0.5f);
@@ -198,6 +239,11 @@ public class MemoryController : MonoBehaviour {
 							Debug.Log("seleccionat "+hit.transform.gameObject.name);
 							selected_item = hit.transform.gameObject;
 							flip_selected=true;
+
+							selected_flip_controller = (FlipController)selected_item.GetComponent(typeof(FlipController));
+							selected_flip_controller.FlipCard();
+
+							selected_txt.text = selected_item.name;
 						}
 						else Debug.Log("skipped select");
 					}
@@ -207,6 +253,12 @@ public class MemoryController : MonoBehaviour {
 						{
 							Debug.Log(hit.transform.gameObject.name+" <> "+selected_item.name);
 							Debug.Log(hit.transform.gameObject.transform.position+" <> "+selected_item.transform.position);
+
+							hit_txt.text = hit.transform.gameObject.name;
+
+							hit_flip_controller = (FlipController)hit.transform.gameObject.GetComponent(typeof(FlipController));
+							hit_flip_controller.FlipCard();
+
 							if(hit.transform.gameObject.name==selected_item.name)
 							{
 								Debug.Log("hit");
@@ -216,9 +268,19 @@ public class MemoryController : MonoBehaviour {
 							{
 								Debug.Log("miss");
 								marcador_misses++;
+
+								//flip back selected
+								selected_flip_controller = (FlipController)selected_item.GetComponent(typeof(FlipController));
+								selected_flip_controller.FlipCard();
+
+								//flip back hit
+								hit_flip_controller = (FlipController)hit.transform.gameObject.GetComponent(typeof(FlipController));
+								hit_flip_controller.FlipCard();
 							}
+
 							selected_item=null;
 							flip_selected=true;
+							selected_txt.text="NULL";
 						}
 					}
 				}
